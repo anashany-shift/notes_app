8d13a36d9235f0c9062380ca49faa3d89d2af9f4
anashany <anashany1290@gmail>
2025-04-13 14:19:46 +0200

    create note App change theme and create note_view create notesViewBody create appBar

---
+++ lib/views/widgets/custom_app_bar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'custom+Search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text('Notes',style: TextStyle(
+          color: Colors.white,fontSize: 28
+        ),),
+        Spacer(),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
+
