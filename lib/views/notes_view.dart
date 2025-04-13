6024c3cbb92fd748d2db5f02e02f242c442e295c
anashany <anashany1290@gmail>
2025-04-13 23:45:58 +0200

    create note App change theme and create note_view create notesViewBody create appBar create custom_note_item create notes_list_view add floating action button

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -7,7 +7,13 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:NotesViewBody() ,
+      floatingActionButton: FloatingActionButton(
+        shape: CircleBorder(),
+        backgroundColor: Colors.cyan,
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      body: NotesViewBody(),
     );
   }
 }
