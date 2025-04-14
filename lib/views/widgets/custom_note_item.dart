66b8c9e9ae319297b690bfecbd92250034b3f38d
anashany <anashany1290@gmail>
2025-04-14 21:47:21 +0200

    create note App change theme and create note_view create notesViewBody create appBar create custom_note_item create notes_list_view add floating action button create bottom sheet create custom Textfield add fields to bottom sheet add custom button create edit note view and navigate to edit view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,42 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 24,bottom: 24,left: 16),
-      decoration: BoxDecoration(
-        color: Colors.yellowAccent,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return EditNoteView();
+        },));
+      },
+      child: Container(
+        padding: EdgeInsets.only(top: 24,bottom: 24,left: 16),
+        decoration: BoxDecoration(
+          color: Colors.yellowAccent,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
 
-            title: Text(
-              'Flutter Tips',
-              style: TextStyle(fontSize: 26),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16.0,bottom: 16),
-              child: Text(
-                'build your career with Anas',
-                style: TextStyle(fontSize: 20,color: Colors.black.withOpacity(0.5)),
+              title: Text(
+                'Flutter Tips',
+                style: TextStyle(fontSize: 26),
               ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16.0,bottom: 16),
+                child: Text(
+                  'build your career with Anas',
+                  style: TextStyle(fontSize: 20,color: Colors.black.withOpacity(0.5)),
+                ),
+              ),
+              trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete,size:30,)),
             ),
-            trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete,size:30,)),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24.0),
-            child: Text(
-              'May21 , 2025',
-              style: TextStyle(color: Colors.black.withOpacity(0.4)),
-            ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: Text(
+                'May21 , 2025',
+                style: TextStyle(color: Colors.black.withOpacity(0.4)),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
