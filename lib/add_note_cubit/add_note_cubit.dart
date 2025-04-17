77079d9b7b3d78d9c0b0d0e1bed4c657a435e4ad
anashany <anashany1290@gmail>
2025-04-17 22:42:11 +0200

    create note App change theme and create note_view create notesViewBody create appBar create custom_note_item create notes_list_view add floating action button create bottom sheet create custom Textfield add fields to bottom sheet add custom button create edit note view and navigate to edit view add custom app bar to edit view add textfield to edit view reformate code and create note model add hive package use hive in model and generate code init hive and open note box take input and validate data create cubit add note state add note method to add note cubit

--- lib/add_note_cubit/add_note_cubit.dart
+++ lib/add_note_cubit/add_note_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app/constant.dart';
+import 'package:note_app/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box(kNotesBox);
+      await notesBox.add(note);
+      emit(AddNoteSuccess());
+    } on Exception catch (e) {
+      AddNoteFailure(e.toString());
+    }
+  }
 }
