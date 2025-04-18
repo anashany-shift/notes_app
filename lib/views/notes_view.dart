85a5f08c995757acf2ed4ac97a7462e019e65354
anashany <anashany1290@gmail>
2025-04-18 21:09:16 +0200

    create note App change theme and create note_view create notesViewBody create appBar create custom_note_item create notes_list_view add floating action button create bottom sheet create custom Textfield add fields to bottom sheet add custom button create edit note view and navigate to edit view add custom app bar to edit view add textfield to edit view reformate code and create note model add hive package use hive in model and generate code init hive and open note box take input and validate data create cubit add note state add note method to add note cubit add add note cubit to add notr bottom sheet add simple bloc observer refactor place of multi bloc provider and remove from main to bottom sheet file change loading in bottom sheet fix keyboard issue in bottom sheet create notes cubit

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:note_app/views/widgets/bottom_sheet.dart';
 import 'package:note_app/views/widgets/notes_view_body.dart';
 
@@ -7,24 +9,27 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        shape: CircleBorder(),
-        backgroundColor: Colors.cyan,
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          shape: CircleBorder(),
+          backgroundColor: Colors.cyan,
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
 
-            backgroundColor: Color(0xff242222),
-            context: context,
-            builder: (context) {
-              return AddNoteBottomSheet();
-            },
-          );
-        },
-        child: Icon(Icons.add),
+              backgroundColor: Color(0xff242222),
+              context: context,
+              builder: (context) {
+                return AddNoteBottomSheet();
+              },
+            );
+          },
+          child: Icon(Icons.add),
+        ),
+        body: NotesViewBody(),
       ),
-      body: NotesViewBody(),
     );
   }
 }
--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:note_app/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app/models/note_model.dart';
 
+import '../../cubits/add_note_cubit/add_note_cubit.dart';
 import 'csutom_button.dart';
 import 'custom_text_field.dart';
 
