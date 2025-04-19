95ea80b4b21bab651c2014061219e0bf4fa99ade
anashany <anashany1290@gmail>
2025-04-19 12:39:07 +0200

    create note App change theme and create note_view create notesViewBody create appBar create custom_note_item create notes_list_view add floating action button create bottom sheet create custom Textfield add fields to bottom sheet add custom button create edit note view and navigate to edit view add custom app bar to edit view add textfield to edit view reformate code and create note model add hive package use hive in model and generate code init hive and open note box take input and validate data create cubit add note state add note method to add note cubit add add note cubit to add notr bottom sheet add simple bloc observer refactor place of multi bloc provider and remove from main to bottom sheet file change loading in bottom sheet fix keyboard issue in bottom sheet create notes cubit display notes part 1

--- lib/views/widgets/notes_item_list_view.dart
+++ lib/views/widgets/notes_item_list_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
+import 'package:note_app/models/note_model.dart';
 
 import 'custom_note_item.dart';
 
@@ -8,18 +11,23 @@ class NotesItemListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
-      child: ListView.builder(
-        padding: EdgeInsets.zero,
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: NoteItem(),
-          );
-        },
-      ),
-    ));
+        child: BlocBuilder<NotesCubit, NotesState>(
+          builder: (context, state) {
+            List<NoteModel>notes=BlocProvider.of<NotesCubit>(context).notes ?? [];
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: ListView.builder(
+                padding: EdgeInsets.zero,
+                itemCount:notes.length ,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: NoteItem(),
+                  );
+                },
+              ),
+            );
+          },
+        ));
   }
 }
